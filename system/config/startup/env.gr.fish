badf2edaa6c15a9882ab2f2a747ce154ddef35e1
GregRos <life.gregr@gmail.com>
2022-03-09 23:01:56 +0200

    Added things

--- system/config/startup/env.gr.fish
+++ system/config/startup/env.gr.fish
@@ -1,12 +1,13 @@
 begin
     fish_add_path -P /usr/local/go/bin \
-        "/home/gr/.config/yarn/global/node_modules/.bin" \
-        "/home/gr/.local/bin" \
-        "/home/gr/.fnm" \
-        /home/gr/scripts
+        "$HOME/.config/yarn/global/node_modules/.bin" \
+        "$HOME/.local/bin" \
+        "$HOME/.fnm" \
+        "$HOME/grwsl/system/tools" \
+        "$HOME/grwsl/apps"
 
     set -gx WINTOOLS (wslpath -u 'C:\Users\GregRosenbaum\Dropbox\__Wintools')
-    set -gx GOPATH /home/gr/go
+    set -gx GOPATH $HOME/go
     set -gx NPM_TOKEN 64124ae3-05e2-4298-805c-1d8b19a4c2b3
     if status --is-login
         sudo service dbus start
--- system/config/startup/wm.gr.fish
+++ system/config/startup/wm.gr.fish
@@ -3,11 +3,9 @@ begin
     set -gx KDEDIR /usr
     set -gx QT_STYLE_OVERRIDE breeze
     set -gx XCURSOR_SIZE 64
-    set -gx QT_SCALE_FACTOR 0.9
+    set -gx QT_SCALE_FACTOR 0.8
     set -gx QT_FONT_DPI 196
     # We need to rexport it here for some reason
     set -gx DISPLAY "$DISPLAY"
-    if test "$BIG_MONITOR" = 1
-        set -gx GDK_DPI_SCALE 1.5
-    end
+    set -gx GDK_SCALE 2
 end
