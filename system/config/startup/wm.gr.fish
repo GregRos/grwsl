80f890d2952cfa19f5bf7d792bd42b55f36d888e
GregRos <life.gregr@gmail.com>
2022-03-11 04:50:32 +0200

    Added more things

--- system/config/startup/wm.gr.fish
+++ system/config/startup/wm.gr.fish
@@ -1,11 +1,19 @@
 begin
+    set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2}')":0"
     set -gx XDG_RUNTIME_DIR /tmp/runtime-gr/
     set -gx KDEDIR /usr
     set -gx QT_STYLE_OVERRIDE breeze
-    set -gx XCURSOR_SIZE 64
-    set -gx QT_SCALE_FACTOR 0.8
-    set -gx QT_FONT_DPI 196
-    # We need to rexport it here for some reason
-    set -gx DISPLAY "$DISPLAY"
-    set -gx GDK_SCALE 2
+
+    if test "$BIG_MONITOR" = 1
+        set -gx GDK_DPI_SCALE 1.3
+        set -gx QT_SCALE_FACTOR 0.8
+        set -gx QT_FONT_DPI 192
+        set -gx XCURSOR_SIZE 64
+    else
+        set -gx GDK_DPI_SCALE 1.0
+        set -gx QT_SCALE_FACTOR 1.3
+        set -gx QT_FONT_DPI 92
+        set -gx XCURSOR_SIZE 32
+    end
+
 end
