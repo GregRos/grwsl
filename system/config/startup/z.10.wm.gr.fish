5a59ba9bc81a684c5dc666998c9bcc2d23b3030b
GregRos <life.gregr@gmail.com>
2022-05-17 15:56:45 +0300

    Fixed to X11, added convenience scripts

--- system/config/startup/z.10.wm.gr.fish
+++ system/config/startup/z.10.wm.gr.fish
@@ -4,9 +4,9 @@ begin
         set -gx DISPLAY (gr-hostip):0
         set -gx QT_STYLE_OVERRIDE breeze
         if test "$BIG_MONITOR" = 1
-            set -gx GDK_DPI_SCALE 1.3
+            set -gx QT_FONT_DPI 184
+            set -gx GDK_DPI_SCALE 0.8
             set -gx QT_SCALE_FACTOR 0.8
-            set -gx QT_FONT_DPI 192
             set -gx XCURSOR_SIZE 64
         else
             set -gx GDK_DPI_SCALE 1.3
